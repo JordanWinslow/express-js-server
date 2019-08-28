c3b33311aa4d1de556f41c6dc379017b048ae4da
JordanWinslow <admin@jordanwinslow.me>
2019-08-28 16:57:46 -0700

    added express routing logic

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require("express")
-// including body-parser manually because historically it has been removed from node.js multiple times in the past.
 const bodyParser = require("body-parser")
-
 const PORT = 3000
 
 /* -------------------------------------*\
@@ -14,19 +12,22 @@ const PORT = 3000
 
 const app = express()
 
-// middleware executes top to bottom, so global routing rules, parsing, etc. should be applied first 
+// middleware executes top to bottom, so global routing rules, parsing, etc. should be applied first
 
-app.use()
+app.use(bodyParser.urlencoded({extended: false}))
 
 // ---------------- ROUTING ----------------- //
 app.use("/add-product", (request, response, next) => {
-    response.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</form>')
-    next()
+    response.send('<form action="/product" method="POST"><input type="text" name="productName"><button type="submit">Add Product</form>')
+})
+// use .post instead of .use to limit the logic to get requests only
+app.post("/product", (request, response, next) => {
+    console.log(request.body)
+    response.redirect("/")
 })
 
-app.use("/product", (request, response, next) => {
-
-    response.redirect("/")
+app.use("/", (request, response, next) => {
+    response.send('<h1>Demo Store Homepage!</h1><p><a href="/add-product">Click Here</a> to see express.js routing in action! (note the output will appear in console this time)</p>')
 })
 
 // runs http.createServer and listens in one line so we no longer require("http") or assign a server to a variable before listening. 
